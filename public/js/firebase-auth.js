293a6c3701e46708ef6d3a789bf7aafb1561fed4
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2018-11-10 10:09:07 -0500

    Update firebase-auth.js

--- public/js/firebase-auth.js
+++ public/js/firebase-auth.js
@@ -1,12 +1,4 @@
-var config = {
-    apiKey: "AIzaSyBHNSl5V6unygEvNolhJEUSOkJLthLASWY",
-    authDomain: "trabalho-ingles.firebaseapp.com",
-    databaseURL: "https://trabalho-ingles.firebaseio.com",
-    projectId: "trabalho-ingles",
-    storageBucket: "trabalho-ingles.appspot.com",
-    messagingSenderId: "449682962345"
-};
-  firebase.initializeApp(config);
+
   
   //escutando status do firebase
   firebase.auth().onAuthStateChanged(function(user) {

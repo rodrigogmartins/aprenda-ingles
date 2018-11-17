7bf4eaab0aad5b0a743fa9c30b8f93adada84e1d
rodrigogmartins <rodrigo.martins7121@gmail.com>
2018-11-17 19:13:35 -0200

    refatorando e adicionando back-end de excluir atividades)

--- public/js/config.firebase.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyBHNSl5V6unygEvNolhJEUSOkJLthLASWY",
-    authDomain: "trabalho-ingles.firebaseapp.com",
-    databaseURL: "https://trabalho-ingles.firebaseio.com",
-    projectId: "trabalho-ingles",
-    storageBucket: "trabalho-ingles.appspot.com",
-    messagingSenderId: "449682962345"
-  };
-  
